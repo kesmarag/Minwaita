5582c72e0c6959fc1d45dd82e6b0b279e9487095
Aaron <godlyranchdressing@gmail.com>
2017-05-15 22:18:09 -0400

    Fix script

--- compile.sh
+++ compile.sh
@@ -8,9 +8,9 @@ for variant in '' '-Vanilla' '-OSX'; do
   sassc -t expanded gnome-shell/gnome-shell.scss $COMPILED_FOLDER/Minwaita${variant}/Minwaita${variant}/gnome-shell/gnome-shell.css
 done
 for variant in '' '-Vanilla' '-OSX'; do
-  sassc -t expanded gtk-3.0/gtk${button,,}.scss $COMPILED_FOLDER/Minwaita${variant}/Minwaita${variant}/gtk-3.0/gtk.css
-  sassc -t expanded gtk-3.0/gtk${button,,}-dark.scss $COMPILED_FOLDER/Minwaita${variant}/Minwaita${variant}/gtk-3.0/gtk-dark.css
+sassc -t expanded gtk-3.0/gtk${variant,,}.scss $COMPILED_FOLDER/Minwaita${variant}/Minwaita${variant}/gtk-3.0/gtk.css
+  sassc -t expanded gtk-3.0/gtk${variant,,}-dark.scss $COMPILED_FOLDER/Minwaita${variant}/Minwaita${variant}/gtk-3.0/gtk-dark.css
   for color in '-Dark'; do
-    sassc -t expanded gtk-3.0/gtk${button,,}-dark.scss $COMPILED_FOLDER/Minwaita${variant}/Minwaita${variant}${color,}/gtk-3.0/gtk.css
+    sassc -t expanded gtk-3.0/gtk${variant,,}-dark.scss $COMPILED_FOLDER/Minwaita${variant}/Minwaita${variant}${color,}/gtk-3.0/gtk.css
   done
 done
